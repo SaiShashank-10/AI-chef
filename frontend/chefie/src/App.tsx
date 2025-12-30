4e395e4eab8788d57ce3e0f13ba4bdf9006029aa
Sai Shashank Vakkalanka <vsaishashank10@gmail.com>
2025-12-30 20:09:26 +0530

    day2

---
+++ frontend/chefie/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white text-3xl font-bold">
+      Tailwind v4 is Working 🚀
+    </div>
+  );
+}
